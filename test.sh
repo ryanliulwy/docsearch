447084086a04fe70ce936789687c3e0f29811394
ryanliulwy <ryl001@ucsd.edu>
2023-02-02 11:06:39 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-local $ javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-local $ java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestDocSearch
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestDocSearch
